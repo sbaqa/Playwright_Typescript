efc61a66abfb1445a9482753dfe0d82c76a8166c
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-09-11 18:33:58 +0300

    Create contactUsPageLocators.ts

---
+++ src/locators/contactUsPageLocators.ts
@@ -0,0 +1,13 @@
+export const ContactUsLocators = {
+
+    contactUsPageUrl: 'https://automationexercise.com/contact_us',
+    contactFormHeader: 'h2:has-text("Get In Touch")',
+    statusOfSubmittedFormCss: '.status',
+    firstNameCss : '[placeholder="Name"]',
+    emailCss : '[placeholder="Email"]',
+    subjectCss: '[placeholder="Subject"]',
+    messageCss: '[placeholder="Your Message Here"]',
+    submitFormButtonCss: '.submit_form',
+    textButton: 'Submit',
+    
+}
