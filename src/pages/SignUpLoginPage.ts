37ffb8e2a904d3380dce9a1f0a2bae6557d7ff9d
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-09-17 14:02:47 +0300

    Create SignUpLoginPage.ts

---
+++ src/pages/SignUpLoginPage.ts
@@ -0,0 +1,23 @@
+import { expect, Page } from '@playwright/test';
+import { LoginSignupLocators } from '../locators/loginSignUpPageLocators.ts';
+import { BaseWidget } from '../helpers/baseWidget.ts';
+import { Constants } from '../helpers/constants.ts';
+
+export class SignUpLoginPage extends BaseWidget {
+
+  // page: Page;
+  // private widget: BaseWidget;
+
+  constructor(page: Page) {
+    super(page)
+  }
+
+  async assertSignUpLoginPageIsDisplayed() {
+    await this.sleep(3000);
+    await this.page.waitForLoadState('load');
+    await expect(this.page).toHaveURL(LoginSignupLocators.signUpLoginPageUrl, { timeout: 3000 });
+    await this.page.waitForSelector(LoginSignupLocators.signUpLoginFormCss);
+    await expect(this.page.locator(LoginSignupLocators.signUpLoginFormHeaderCss)).toContainText(Constants.loginToAccountHeader);
+  }
+
+}
