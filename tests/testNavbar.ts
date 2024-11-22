968cc878f48d31138d2275f2657ac1e904e5cc5a
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-11-21 18:44:26 -0900

    Update testNavbar.ts
    Updated tests

--- tests/testNavbar.ts
+++ tests/testNavbar.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { HomePage } from '../src/pages/HomePage.ts';
 import { GeneralLocators } from '../src/locators/generalLocators.ts';
 // import { ContactUsPage } from '../src/pages/ContactUsPage.ts';
@@ -8,8 +8,9 @@ let homePage: HomePage; // Declare the homePage variable outside the test block
 test.describe('TopNavigation tests', () => {
 
   test.beforeEach(async ({ page }) => {
-  await page.goto(GeneralLocators.expectedHomePageUrl)
-  homePage = new HomePage(page)  // Initialize the HomePage object
+    await page.goto(GeneralLocators.expectedHomePageUrl)
+    homePage = new HomePage(page)  // Initialize the HomePage object
+    // await homePage.acceptCookies()
   });
   
   test("Open 'Login' page", async () => {
@@ -17,14 +18,31 @@ test.describe('TopNavigation tests', () => {
     loginPage.assertLoginPageIsDisplayed();
   });
 
-  test.fail("Fill 'Contact us' form: test expected to fail", async () => {
-
-    // Open 'Contact Us' Page being on Home Page and verify it was opened
-    let ContactUsPage = await homePage.navigateToContactUsPage();
-    await ContactUsPage.assertContactUsPageIsDisplayed();
-
-    // Fill 'Contact us' form and verify it was filled successfully
-    let contactUsFormFilled = await ContactUsPage.fillContactUsForm();
-    await contactUsFormFilled.assertContactUsFormSuccessStepIsDisplayed();
+  test("Fill Login page form", async () => {
+    let openedLoginPage = await homePage.navigateToLoginPage();
+    openedLoginPage.fillSignInForm();
   });
+// })
+//   test.fail("Fill 'Contact us' form: test expected to fail", async () => {
+
+//     // Open 'Contact Us' Page being on Home Page and verify it was opened
+//     let ContactUsPage = await homePage.navigateToContactUsPage();
+//     await ContactUsPage.assertContactUsPageIsDisplayed();
+
+//     // Fill 'Contact us' form and verify it was filled successfully
+//     let contactUsFormFilled = await ContactUsPage.fillContactUsForm();
+//     await contactUsFormFilled.assertContactUsFormSuccessStepIsDisplayed();
+//   });
+// })
+
+
+test.afterEach(async ({ page, context }, testInfo) => {
+
+  if (testInfo.status !== 'passed') {
+    await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
+  }
+
+  await context.close(); // Close all pages in the context
+  });
+
 })
