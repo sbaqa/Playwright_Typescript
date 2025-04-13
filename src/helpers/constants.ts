8d08a30a567dd23d6962a47c5e374e4975061917
Sviatoslav Bordovski <sbordovski@gmail.com>
2025-04-12 16:11:25 -0900

    Update constants.ts

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,14 @@
 export const Constants = {
-
-    loginToAccountHeader: 'Login to your account',
-    formSubmittedSuccessfully: 'Success! Your details have been submitted successfully.'
-
+    signInHeader: 'Sign In',
+    formSubmittedSuccessfully: 'Success! Your details have been submitted successfully.',
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    subject: 'Automation Testing',
+    password: 'Lorem Ipsum!',
+    stringGenerator: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+    credentialsErrorMessage: 'email or password is invalid',
+    baseURL: 'https://www.saucedemo.com/',
+    expectedCartNames: ["Sauce Labs Backpack", "Sauce Labs Bike Light",],
+    expectedCartPrices: ["$29.99", "$9.99",],
+    expectedCartItems: 2,
 }
